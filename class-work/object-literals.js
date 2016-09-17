4b1da6346ed9aaeed0e3c54baa73d91e9f05411c
John Garcia <johngarcia9110@gmail.com>
2016-09-16 23:32:55 -0400

    updated object literals with dynamically added properties

--- class-work/object-literals.js
+++ class-work/object-literals.js
@@ -3,3 +3,15 @@
 var cat = {name: 'Fluffy', color: 'White'} //object literal, most commonly used way to create an object.
 
 console.log(cat.name); // displays the name property of the cat object in the console.
+
+//we can add properties to objects on the fly
+
+cat.age = 3;
+cat.speak = function(){
+    console.log("MEEOOWWWW")
+}
+
+console.log(cat.age);
+
+cat.speak();
+
