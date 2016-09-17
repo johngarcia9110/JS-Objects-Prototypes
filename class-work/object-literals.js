026de343989e79ddffe9b5af2a6cb308430edf21
John Garcia <johngarcia9110@gmail.com>
2016-09-16 23:17:09 -0400

    completed object literals section

---
+++ class-work/object-literals.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var cat = {name: 'Fluffy', color: 'White'} //object literal, most commonly used way to create an object.
+
+console.log(cat.name); // displays the name property of the cat object in the console.
