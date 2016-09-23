4262af1d24347ba0117ef3af3665e877875b0e77
John Garcia <johngarcia9110@gmail.com>
2016-09-23 11:33:05 -0400

    added example of class based prototype chains

---
+++ class-work/Prototypes/prototypes-with-classes.js
@@ -0,0 +1,21 @@
+'use strict';
+
+class Animal{
+    constructor(voice){
+        this.voice = voice || 'grunt';
+    }
+    speak(){
+        console.log(this.voice);
+    }
+}
+
+class Cat extends Animal {
+    constructor(name, color){
+        super('Meow');
+        this.name = name;
+        this.color = color;
+    }
+}
+
+var fluffy = new Cat('Fluffy', 'White');
+fluffy.speak();
